49b0e856194ee19825a97b0626ecbadfd8048791
Hamid-Ibragimov <hamit.ibragimov@hotmail.com>
2022-05-09 17:43:59 +0200

    my changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,11 +20,18 @@ from homepage.views import homepage_view
 from homepage.views import test_view
 
 
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+
+
 urlpatterns = [
     path('projects/', include('projects.urls')),
 
     path('admin/', admin.site.urls),
     path('', homepage_view, name='home'),
 
-    path('test/', test_view, name='test')
+    path('test/', test_view, name='test'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
